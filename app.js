28d69ebaa4c85adb0f658c205608dd472e56c93c
ratan tupte <development@tlminsidesales.com>
2022-10-27 13:26:20 +0530

    fdf

--- app.js
+++ app.js
@@ -1,15 +1,69 @@
+const fs = require('fs');
 const express = require('express');
-const appp = express();
+const app = express();
+//middle ware for post
 
-// ROUTING
+app.use(express.json());
 
-appp.get ('/', (req, res) => {
-  // res.end('hiii from server ');
-  res.json({ message: 'hiii from server ', app: 'natours' });
+// ROUTING demo
+
+// appp.get('/', (req, res) => {
+//   // res.end('hiii from server ');
+//   res.json({ message: 'hiii from server ', app: 'natours' });
+// });
+
+const datatour = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+//http  get handler
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'suscess',
+    result: datatour.length, // this is exceptionalgeting length of array this is convient top user
+    data: {
+      tours: datatour, //here we can write  tours only if datatours is tours  same anme as api end point api/v1/tours
+    },
+  });
+});
+
+// post handler
+
+// app.post('/api/v1/tours', (req, res) => {
+//   console.log(req.body);
+//   res.end('done');
+// });
+
+//post save request in on tours-simple.json file
+app.post('/api/v1/tours', (req, res) => {
+  //create id of coming req data  req
+  const newId = datatour[datatour.length - 1].id + 1;
+
+  // store all req datat and assign id to coming  data
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  //push data
+  datatour.push(newTour);
+
+  // data store in json file
+
+  // fs.writeFile(
+  //   `${__dirname}/dev-data/data/tours-simple.json`,
+  //   JSON.stringify(datatour),
+  //   (err) => {
+  res.status(201).json({
+    status: 'suscess',
+
+    data: {
+      tour: newTour,
+    },
+  });
+  //   }
+  // );
 });
 
 // port
 const port = 3000;
-appp.listen(port, () => {
+app.listen(port, () => {
   console.log(`app listenibg on port ${port}`);
 });
