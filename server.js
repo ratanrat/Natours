67142cc50f5cd18b158194f8c29e3dc45e14b9de
ratan tupte <development@tlminsidesales.com>
2022-10-31 18:34:05 +0530

    ratan

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const dotenv = require('dotenv');
 
+const mongose = require('mongoose');
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
@@ -13,3 +15,33 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`app listenibg on port ${port}`);
 });
+
+//db connectivity
+
+// const dbstring = process.env.DATABASE.replace(
+//   '<PASSWORD>',
+//   process.env.DATABASE_PASSWORD
+// );
+
+// HOSTED DB CONNECTION
+// const dbstring = process.env.DATABASE.replace(
+//   '<PASSWORD>',
+//   process.env.DATABASE_PASSWORD
+// );
+
+// mongose
+//   .connect(dbstring, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false
+//   })
+//   .then(() => console.log('DB connection successful!'));
+
+// LOCAL DB CONNECTING
+mongose
+  .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connection successful!'));
