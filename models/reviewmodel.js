7349f546e5b881db5061e62244ee5e46d7c54d4d
ratan tupte <development@tlminsidesales.com>
2022-12-01 16:40:50 +0530

    upto geolocaion

--- models/reviewmodel.js
+++ models/reviewmodel.js
@@ -1,4 +1,5 @@
 const mongose = require('mongoose');
+const Tour = require('./tourmodel');
 
 const reviewschema = new mongose.Schema(
   {
@@ -31,6 +32,10 @@ const reviewschema = new mongose.Schema(
     toObject: { virtuals: true }
   }
 );
+reviewschema.index({ tour: 1, user: 1 });
+
+// reviewschema.index({ tour: 1, user: 1 }, { unique: true });
+
 // multipule populate
 // reviewschema.pre(/^find/, function(next) {
 //   this.populate({
@@ -40,11 +45,65 @@ const reviewschema = new mongose.Schema(
 //   next();
 // });
 
+// calculating averarge of rating and store it in tour model using statics method
+
+reviewschema.statics.calculateaveragerating = async function(tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId }
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgrating: { $avg: '$rating' }
+      }
+    }
+  ]);
+  console.log(stats);
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgrating
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5
+    });
+  }
+};
+
+// middeleawre to call function
+// using post beacuse we want to call on it when review stored each  the time must be updated
+
+reviewschema.post('save', function() {
+  this.constructor.calculateaveragerating(this.tour); // and using this.constructor is used access of current tour and reason is we use post middleware
+});
+
+//we d0ont have any middleware for folwing this query
+// findByIdAndUpdate
+// findByIdAndDelete
+// therfore for that we use query middle ware whenever findByIdAndUpdate findByIdAndDelete is fire
+// fineoneand=findByIdAnd therefore this will exicute on evry findByIdAndUpdat findByIdAndDeletem query
+reviewschema.pre(/^findOneAnd/, async function(next) {
+  this.r = await this.findOne(); //this.r is used to store it on currentdocu so post middleware can get access the curent document beacuse post middleware exicute after query done
+  // console.log(this.r);
+  next();
+});
+
+reviewschema.post(/^findOneAnd/, async function() {
+  // await this.findOne(); does NOT work here, query has already executed therfore we use this.r to get info about tour
+  // constructor for apply on current
+  await this.r.constructor.calculateaveragerating(this.r.tour);
+});
+
 // we did single for tour when we fetch we want only review populate only user
 reviewschema.pre(/^find/, function(next) {
   this.populate({ path: 'user', select: 'name photo' });
   next();
 });
+
 const reviewdb = mongose.model('review', reviewschema);
 
 module.exports = reviewdb;
